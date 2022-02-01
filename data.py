f3ce1fe3080f082a1ae042f7f3eb4a31ffd3ee5a
douglaslimab <douglaslimab@gmail.com>
2022-02-01 20:35:43 +0000

    covid Dashboard

--- data.py
+++ data.py
@@ -1,8 +1,12 @@
 import pandas as pd
+from flask import Flask
+
+app = Flask(__name__)
 
 df = pd.read_csv("/Users/dougl/OneDrive/Documents/PortfolioDashboard/covidData.csv")
 
-country = 'Brazil'
+#country = 'Brazil'
+
 
 def get_table(country):
     df_output = df.loc[df['Country_Region'] == country][['Province_State', 'Country_Region', 'Confirmed', 'Lat', 'Long_']]
@@ -14,8 +18,37 @@ def get_table(country):
 def get_sumary(country):
     df_country = df.groupby(['Country_Region']).sum()
     df_country = df_country[['Confirmed', 'Deaths', 'Recovered', 'Active']]
-    return df_country.loc[country]
+
+    dct = {
+        "Country": country,
+        "Confirmed": df_country.loc[country][0],
+        "Deaths": df_country.loc[country][1],
+        "Recovered": df_country.loc[country][2],
+        "Active": df_country.loc[country][3]
+    }
+    return dct
+
+def get_geo(country):
+    df_country = df.groupby(['Country_Region']).mean()
+    df_country = df_country[['Lat', 'Long_', 'Case_Fatality_Ratio']]
+
+    dct = {
+        "Country": country,
+        "Latitude": df_country.loc[country][0],
+        "Longitude": df_country.loc[country][1],
+        "Fatality_ratio": df_country.loc[country][2]
+    }
+    return dct
 
 
-get_table(country)
-print(get_sumary(country))
+@app.route('/<country>')
+def home(country):
+    response = get_geo(country)
+    return response
+
+
+#get_table(country)
+#print(get_sumary(country))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ num.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+arr = np.zeros(shape=(2, 3, 4))
+
+print(arr)
